6943672e24bc43e59d777acf9e5b87de82ea2070
michaelhaskell00 <91441329+michaelhaskell00@users.noreply.github.com>
2021-12-17 05:37:25 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(500,500);
+  frameRate(120);
+  rectMode(CENTER);
+}
+
+function draw() {
+  let step = frameCount % 10;
+  background(0);
+  translate(80,80);
+    applyMatrix(1 / step, 0, 0, 1 / step, 0, 0);
+  rect(0, 0, 50, 50);
+  fill(0,0,255);
+  stroke(255,153,0);
+  strokeWeight(2);
+  ellipse(50,100,20,20);
+  fill(255,153,0);
+  stroke(0,0,255);
+  ellipse(250,250,40,40);
+  fill(0,255,0);
+  stroke(0,0,255);
+  strokeWeight(6); ellipse(random(width),random(height),40,40);
+  ellipse(200,200,20,20);
+  fill(0);
+  stroke(255,0,0);
+  strokeWeight(8); ellipse(random(width),random(height),20,20);
+  ellipse(170,170,60,60);
+  fill(123,255,230);
+  stroke(200,0,255);
+  strokeWeight(4); ellipse(random(width),random(height),30,30);
+    ellipse(290,290,80,80);
+  fill(240,255,0);
+  stroke(255,0,0);
+  strokeWeight(5); ellipse(random(width),random(height),70,70);
+}
+
